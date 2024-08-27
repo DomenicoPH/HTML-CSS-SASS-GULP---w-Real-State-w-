c6e8cbbeb7662f8a9796b7fdd5d75f4f3c098d8c
Domenico Pagano <dpaganoh@gmail.com>
2024-08-26 22:42:41 -0500

    js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//alert('Bienvenido a Real State');
+
+const imagenes = document.querySelectorAll('.propiedad__imagen');
+console.log(imagenes);
+
+window.addEventListener('scroll', () => {
+    //console.log(this.scrollY);
+
+    const scroll = this.scrollY / 20;
+
+    imagenes.forEach( (imagen) => {
+        imagen.style.backgroundPositionY = `${scroll}px`;
+    })
+    
+})
